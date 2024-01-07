53ae01a61aa1ff7dc57620bbd22dd08f5af52737
rajorshi-mandal <raj86969051@gmail.com>
2024-01-07 11:44:06 +0530

    Recursion Continuation : RemoveDup (iterative, recursive), friendsPair, printBinIntStrings, towerOfHanoi, countSubStringsFirstEndSame, convertIntToStrings, stringLength. Recursion Completed

--- Java_DSA_Practice/Recursion/recursion.java
+++ Java_DSA_Practice/Recursion/recursion.java
@@ -106,6 +106,109 @@ public class recursion {
         return tilling(n - 1) + tilling(n - 2);
     }
 
+    //iterative approach
+    // public static String removeDup(String str){
+    //     StringBuilder sb = new StringBuilder("");
+
+    //     int[] arr = new int[26];
+
+    //     for(int i = 0; i < str.length(); i++){
+    //         char ch = str.charAt(i);
+    //         if(arr[ch - 'a'] == 0){
+    //             arr[ch - 'a']++;
+    //             sb.append(ch);
+    //         } else {
+    //             arr[ch - 'a']++;
+    //         }
+    //     }
+
+    //     return sb.toString();
+    // }
+
+    public static String removeDup(String str, int i, StringBuilder newStr, boolean[] map){
+
+        if(i == str.length()){
+            // System.out.println(newStr);
+            return newStr.toString();
+        }
+
+        char ch = str.charAt(i);
+        if(map[ch - 'a'] == true){
+            removeDup(str, i + 1, newStr, map);
+        } else {
+            newStr.append(ch);
+            map[ch - 'a'] = true;
+            removeDup(str, i, newStr, map);
+        }
+
+        return newStr.toString();
+    }
+
+    public static int friendsPair(int n){
+        if(n == 1 || n == 2){
+            return n;
+        }
+
+        return friendsPair(n - 1) + (n - 1) * friendsPair(n - 2);
+    }
+
+    public static void printBinIntStrings(int n, String str, int lastPlace){
+        if(n == 0){
+            System.out.println(str);
+            return;
+        }
+
+        printBinIntStrings(n - 1,str + "0", 0);
+        if(lastPlace == 0){
+            printBinIntStrings(n - 1, str + "1", 1);
+        }
+    }
+
+    public static void towerOfHanoi(int n, String src, String helper, String dest){
+        if(n == 0){
+            return;
+        }
+
+        towerOfHanoi(n - 1, src, dest, helper);
+        System.out.println("move " + src + " to " + dest);
+        towerOfHanoi(n - 1, helper, src, dest);
+    }
+
+
+    public static int countSubStringsFirstEndSame(String str, int i, int j, int n){
+        if(n == 1){
+            return 1;
+        }
+
+        if(n <= 0){
+            return 0;
+        }
+
+        int count = countSubStringsFirstEndSame(str, i + 1, j, n - 1) + countSubStringsFirstEndSame(str, i, j - 1, n - 1) - countSubStringsFirstEndSame(str, i + 1, j - 1, n - 2);
+        if(str.charAt(i) == str.charAt(j)){
+            count++;
+        }
+
+        return count;
+    }
+
+    public static void convertIntToString(int n, String[] arr){
+        if(n == 0){
+            return;
+        }
+
+        convertIntToString(n / 10, arr);
+        System.out.print(arr[n % 10]);
+    }
+
+    public static int stringLength(String str){
+        if(str.length() == 0){
+            return 0;
+        }
+
+        return stringLength(str.substring(1)) + 1;
+    }
+
     public static void main(String[] args) {
         // printNOne(10);
         // System.out.println(factorial(4));
@@ -117,6 +220,23 @@ public class recursion {
         // System.out.println(firstOcc(arr, 7, 0));
         // System.out.println(powX(2, 3));
         // System.out.println(powXlogn(3, 3));
-        System.out.println(tilling(5));
+        // System.out.println(tilling(5));
+        StringBuilder sb = new StringBuilder("");
+        boolean[] map = new boolean[26];
+        // System.out.println(removeDup("appnnacollegge",0,sb, map));
+
+        // System.out.println(friendsPair(4));
+        // printBinIntStrings(3, "", 0);
+        // towerOfHanoi(3, "A", "B", "C");
+        String str = "abcab";
+        // System.out.println(countSubStringsFirstEndSame(str, 0, str.length() - 1, str.length()));
+
+        String[] numberStrings = {
+            "Zero", "One", "Two", "Three", "Four",
+            "Five", "Six", "Seven", "Eight", "Nine"
+        };
+
+        // convertIntToString(174, numberStrings);
+        System.out.println(stringLength("rcciit"));
     }
 }
