8cd3a33ba35d3fae248da3d5c195bdc9a813fc8c
rajorshi-mandal <raj86969051@gmail.com>
2024-08-01 12:24:49 +0530

    managing minor changes in files

---
+++ Meity_Course/stu_info.py
@@ -0,0 +1,6 @@
+import main as mp
+mp.student("Arjun", 88, 45)
+
+print("Palindrome") if mp.isPalindrome(233) else print("Not Palindrome")
+
+print("krishnamurti") if mp.krishnamurti(135) else print("Not krishnamurti")
